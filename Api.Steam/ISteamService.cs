2bcd43e93bba54dc4c73328e1b396373cf2f8a63
DBC <lancer1977@gmail.com>
2025-10-08 00:51:53 -0400

    tweaks

--- Api.Steam/ISteamService.cs
+++ Api.Steam/ISteamService.cs
@@ -4,8 +4,46 @@ namespace PolyhydraGames.Api.Steam;
 
 public interface ISteamService
 {
-    public Task<GameResult> GetGameData(int id, bool forceRefresh = false);
-    public Task<GameResult> GetGameData(string id,bool forceRefresh = false); 
-    Task<IEnumerable<SimpleSteamGame>> GetSimpleGames(string filter, int count = 50, bool forceRefresh = false);
 
+    // ------------------------------------------------------------------------
+    //  Game Data
+    // ------------------------------------------------------------------------
+    Task<GameResult> GetGameData(string name, bool forceRefresh = false);
+    Task<GameResult> GetGameData(int id, bool forceRefresh = false);
+
+    // ------------------------------------------------------------------------
+    //  App List
+    // ------------------------------------------------------------------------
+    Task<IEnumerable<SimpleSteamGame>> GetSimpleGames(string filter, int count = 50, bool forceRefresh = false);
+    Task<List<SimpleSteamGame>> GetSimpleGamesCached(bool forceRefresh = false);
+
+    // ------------------------------------------------------------------------
+    //  ISteamNews
+    // ------------------------------------------------------------------------
+    Task<GetNewsForAppResponse?> GetNewsForAppAsync(int appId, int count = 3, int maxLength = 300);
+
+    // ------------------------------------------------------------------------
+    //  ISteamUserStats
+    // ------------------------------------------------------------------------
+    Task<GetGlobalAchievementPercentagesResponse?> GetGlobalAchievementPercentagesForAppAsync(int appId);
+    Task<GetPlayerAchievementsResponse?> GetPlayerAchievementsAsync(string steamId, int appId, string language = "en");
+    Task<GetUserStatsForGameResponse?> GetUserStatsForGameAsync(string steamId, int appId, string language = "en");
+
+    // ------------------------------------------------------------------------
+    //  ISteamUser
+    // ------------------------------------------------------------------------
+    Task<GetPlayerSummariesResponse?> GetPlayerSummariesAsync(IEnumerable<string> steamIds);
+    Task<GetFriendListResponse?> GetFriendListAsync(string steamId, string relationship = "friend");
+
+    // ------------------------------------------------------------------------
+    //  IPlayerService
+    // ------------------------------------------------------------------------
+    Task<GetOwnedGamesResponse?> GetOwnedGamesAsync(string steamId, bool includeAppInfo = true, bool includePlayedFreeGames = false);
+    Task<GetRecentlyPlayedGamesResponse?> GetRecentlyPlayedGamesAsync(string steamId, int count = 0);
+
+    // ------------------------------------------------------------------------
+    //  ISteamWebAPIUtil
+    // ------------------------------------------------------------------------
+    Task<GetSupportedAPIListResponse?> GetSupportedAPIListAsync();
 }
+
